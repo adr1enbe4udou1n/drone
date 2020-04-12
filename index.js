33232ee039bf24341c6edd3d8435c12147736ebe
Adrien Beaudouin <adrien.beaudouin@outlook.com>
2020-04-12 21:44:42 +0200

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const slugify = require('slugify');
+
+const port = parseInt(process.env.PORT || '3000', 10);
+
+let express = require('express'),
+  http = require('http'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  childprocess = require('child_process'),
+  path = require('path');
+
+app.use(bodyParser.json());
+
+app.post(process.env.WEBHOOK_PATH || '/deploy', (req, res) => {
+  if (!req.body || !req.body.repository || !req.body.repository.name) {
+    return res.status(400).json({
+      message: 'Invalid request!'
+    });
+  }
+
+  let name = slugify(req.body.repository.name, {
+    lower: true
+  });
+
+  let projectDir = path.join(process.env.PROJECTS_ROOT, name);
+
+  childprocess.exec(`cd ${projectDir} && git pull`);
+
+  const date = new Date();
+  console.log(`${projectDir} pulled at ${date.toString()} !`);
+
+  res.status(200).json({
+    message: 'Git Hook received!'
+  });
+});
+
+http.createServer(app).listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
